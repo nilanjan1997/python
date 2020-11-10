9564dd1d103aa35d5b9697e3d7f755e94195a3d5
NILANJAN <nilanjansantra1402@gmail.com>
2020-11-11 00:51:44 +0530

    python

--- 01_hello.py
+++ 01_hello.py
@@ -1,1 +1,10 @@
 print("Hello World!!!")
+print("wow")
+# i am nilanjan
+''' i am nilanjan '''
+print('''i am chemist''')
+print(""" computational chemistry""")
+# gooooooooooo
+import os # yooo
+'''pondicherry
+university'''
